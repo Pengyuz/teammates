2a302fe944c00b2b48ed877a9a2abf1c06f8a2e2
erjillsison <erjillsison@gmail.com>
2019-06-12 16:40:12 +0800

    Update style and add input form

--- src/web/app/components/comment-box/comment-box.component.ts
+++ src/web/app/components/comment-box/comment-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'tm-comment-box',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentBoxComponent implements OnInit {
 
+  commentText:string = '';
+
+  @Output()
+  closeCommentBox: EventEmitter<any> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  triggerFormChange(data: any) {
+    console.log(data);
+  }
+
+  triggerCloseCommentBox(data: any) {
+    this.closeCommentBox.emit();
+    console.log(data);
   }
 
 }
--- src/web/app/components/comment-box/comment-box.module.ts
+++ src/web/app/components/comment-box/comment-box.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from "@angular/forms";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { RichTextEditorModule } from "../rich-text-editor/rich-text-editor.module";
 import { CommentBoxComponent } from './comment-box.component';
 
@@ -8,6 +10,8 @@ import { CommentBoxComponent } from './comment-box.component';
   imports: [
     CommonModule,
     RichTextEditorModule,
+    NgbModule,
+    FormsModule,
   ],
   exports: [
       CommentBoxComponent,
