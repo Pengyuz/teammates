faaeeace7f88571f0aeae5410dd2fb944a106a89
erjillsison <erjillsison@gmail.com>
2019-06-12 14:39:20 +0800

    Add rich text editor to comment box component

--- src/web/app/components/comment-box/comment-box.module.ts
+++ src/web/app/components/comment-box/comment-box.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RichTextEditorModule } from "../rich-text-editor/rich-text-editor.module";
 import { CommentBoxComponent } from './comment-box.component';
 
 @NgModule({
   declarations: [CommentBoxComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RichTextEditorModule,
   ],
   exports: [
       CommentBoxComponent,
